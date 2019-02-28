811bd9766ce4f2125bce668029c84ef2bb42f8c2
Eric <ericqu96@gmail.com>
2019-02-27 23:35:28 -0800

    changed environment and character parameters for faster testing, added player input to call character movement methods, along with a detailed bug

--- client/js/game.js
+++ client/js/game.js
@@ -9,7 +9,7 @@ let gameState = {
     let groundTile1Data = {
       x: 0,
       y: 500,
-      width: config.gameState.groundTile.x * 10,
+      width: config.gameState.groundTile.x * 3,
       height: config.gameState.groundTile.y,
       spriteKey: config.gameState.groundTile.spriteKey
     };
@@ -17,13 +17,29 @@ let gameState = {
 
     let phaserDudeData = {
       x: 0,
-      y: 0,
+      y: 300,
       spriteKey: config.gameState.phaserDude.spriteKey
     };
     gameState.phaserDude = Platforming.spawnCharacter(phaserDudeData);
   },
 
   update: () => {
-    game.physics.arcade.collide(gameState.phaserDude.sprite, gameState.groundTile1.sprite, ()=>console.log("hit"));
+    // Physics collisions need to be handled BEFORE velocities are set.
+    // The following line needs to effectively run before the rest of the function.
+    // Adding this in the sprite's update method does not do this.
+    // game.physics.arcade.collide(gameState.phaserDude.sprite, gameState.groundTile1.sprite);
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+      gameState.phaserDude.moveLeft();
+    } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+      gameState.phaserDude.moveRight();
+    }
+    else {
+      gameState.phaserDude.sprite.body.velocity.x = 0;  // HACK
+    }
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+      gameState.phaserDude.jump();
+    }
   }
 }
