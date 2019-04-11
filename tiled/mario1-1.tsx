2f1b9222ef1a19c59e5c05cf59af869fcf6d6639
Eric <ericqu96@gmail.com>
2019-04-10 21:45:48 -0700

    created tilemaps of my own through Tiled

---
+++ tiled/mario1-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="mario1-1" tilewidth="16" tileheight="16" tilecount="44" columns="11">
+ <image source="../client/assets/tilemaps/tiles/super_mario.png" width="176" height="64"/>
+</tileset>
